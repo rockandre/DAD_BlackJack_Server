252c10640d719413237af948fd932999ed63a131
Tiago Martins <2151237@my.ipleiria.pt>
2018-01-25 18:44:23 +0000

    player.js with 'private' variables

--- player.js
+++ player.js
@@ -1,30 +1,51 @@
 /*jshint esversion: 6 */
 
 const Card = require('./card.js');
+let hands = new WeakMap();
 
 class BlackJackPlayer {
+
     constructor(Name) {
         this.name = Name;
-        let hand = [];
         this.stand = 0;
         this.pubHand = [];
+        var privateProperties = {
+            hand: []
+        };
+        hands.set(this,privateProperties);
     }
-
+    
     handSum(){
         sum = 0;
-        this.hand.forEach(card => {
+        hands.get(this).hand.forEach(card => {
             sum+=card.value;
         });
         return sum;
     }
 
+    showHand(){
+        //console.log("playerHand: ");
+        //console.log(hands.get(this).hand);
+        return hands.get(this).hand;
+    }
+
     addCard(newCard){
-        hand.push(newCard);
-        if(hand.size()==0){
+        //console.log("New card: "+newCard);
+        //console.log("Player hand: "+hands.get(this).hand);
+        console.log("ARR LENGTH: "+hands.get(this).hand.length);
+        console.log(hands.get(this).hand);
+        if(hands.get(this).hand.length==0){
+            //console.log("weakmap empty");
             this.pubHand.push(newCard);
         } else {
+            //console.log("weakmap not empty");
             this.pubHand.push(new Card("semFace",0));
         }
+        let privatePropertiesChanger = hands.get(this);
+        privatePropertiesChanger.hand.push(newCard);
+        hands.set(this,privatePropertiesChanger);
+        //console.log("added a card, and new hand is: ");
+        //console.log(hands.get(this).hand);
     }
 
 }
